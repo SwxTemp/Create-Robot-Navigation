1fe4088a987137d26d2f05dc36a16c8e8b1edb32
Swx-GIX-Laptop <mhwu@uw.edu>
2024-05-22 17:02:43 -0700

    HeHeHe

--- create_robot/create_navigation/scripts/navigate.py
+++ create_robot/create_navigation/scripts/navigate.py
@@ -1,31 +1,50 @@
 #!/usr/bin/env python
+
 import rospy
 import actionlib
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
-from geometry_msgs.msg import Quaternion
+from geometry_msgs.msg import PoseStamped
 
-def move_to_goal(x, y, z, w):
-    rospy.init_node('send_navigation_goal', anonymous=False)
+def send_goal(x, y, z, w):
+    rospy.init_node('send_goal_node')
 
+    # Create an action client
     client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
     client.wait_for_server()
 
+    # Define the goal
     goal = MoveBaseGoal()
     goal.target_pose.header.frame_id = "map"
     goal.target_pose.header.stamp = rospy.Time.now()
-
     goal.target_pose.pose.position.x = x
     goal.target_pose.pose.position.y = y
     goal.target_pose.pose.orientation.z = z
     goal.target_pose.pose.orientation.w = w
 
+    # Send the goal
     client.send_goal(goal)
-    wait = client.wait_for_result()
+    client.wait_for_result()
 
-    if not wait:
-        rospy.logerr("Action server not available!")
+    # Check if the robot reached its goal
+    if client.get_state() == actionlib.GoalStatus.SUCCEEDED:
+        rospy.loginfo("The robot has reached the goal")
     else:
-        return client.get_result()
+        rospy.loginfo("The robot failed to reach the goal")
+
+    # Publish the goal for RViz
+    pub = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10)
+    goal_msg = PoseStamped()
+    goal_msg.header.frame_id = "map"
+    goal_msg.header.stamp = rospy.Time.now()
+    goal_msg.pose.position.x = x
+    goal_msg.pose.position.y = y
+    goal_msg.pose.orientation.z = z
+    goal_msg.pose.orientation.w = w
+
+    rospy.sleep(1)  # Allow some time for the publisher to set up
+    pub.publish(goal_msg)
+    rospy.loginfo("Goal published to /move_base_simple/goal")
+
 
 if __name__ == '__main__':
     try:
@@ -35,7 +54,7 @@ if __name__ == '__main__':
         # w = float(input("Enter w coordinate (orientation): "))
         z = 0.0
         w = 1.0
-        result = move_to_goal(x, y, z, w)
+        result = send_goal(x, y, z, w)
         if result:
             rospy.loginfo("Navigation success!")
     except rospy.ROSInterruptException:
--- create_teleop/README.md
+++
@@ -1,5 +0,0 @@
-# How to use
-
-```
-roslaunch create_teleop create_teleop.launch
-```
